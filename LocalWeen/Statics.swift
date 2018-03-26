e207e454b0cec5a8635c43ceb86983b2a62a00dc
Unknown <bruce.bookman@gmail.com>
2018-03-26 11:30:02 -0700

    Start Statics

---
+++ LocalWeen/Statics.swift
@@ -0,0 +1,15 @@
+//
+//  Statics.swift
+//  LocalWeen
+//
+//  Created by Bruce Bookman on 3/26/18.
+//  Copyright © 2018 Bruce Bookman. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    static let GServicesKey = "AIzaSyCAL3awSh-YPf9HwawGLjBjukc6Kz9478k"
+    static let GPlacesKey = "AIzaSyD2RJCP9eoFaL3HPPfbYaetg_8BWhXCa24"
+    
+}
