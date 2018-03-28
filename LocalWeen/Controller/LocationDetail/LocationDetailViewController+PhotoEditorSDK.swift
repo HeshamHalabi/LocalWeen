263eed55606484b40f52dc84e7d4209897173efa
Bruce Bookman <bruce.bookman@gmail.com>
2018-03-27 20:15:29 -0700

    Going to give the complete freebee a try

--- LocalWeen/Controller/LocationDetail/LocationDetailViewController+PhotoEditorSDK.swift
+++ LocalWeen/Controller/LocationDetail/LocationDetailViewController+PhotoEditorSDK.swift
@@ -10,13 +10,66 @@ import UIKit
 import PhotoEditorSDK
 import SwiftyBeaver
 
+
+/*
+ 
+ // Get the item[s] we're handling from the extension context.
+ 
+ // For example, look for an image and place it into an image view.
+ // Replace this with something appropriate for the type[s] your extension supports.
+ var imageFound = false
+ for item in self.extensionContext!.inputItems as! [NSExtensionItem] {
+ for provider in item.attachments! as! [NSItemProvider] {
+ if provider.hasItemConformingToTypeIdentifier(kUTTypeImage as String) {
+ // This is an image. We'll load it, then place it in our image view.
+ 
+ provider.loadItem(forTypeIdentifier: kUTTypeImage as String, options: nil, completionHandler: { (data, error) in
+ OperationQueue.main.addOperation {
+ var contentData: Data? = nil
+ 
+ if let data = data as? Data {
+ contentData = data
+ } else if let url = data as? URL {
+ contentData = try? Data(contentsOf: url)
+ }else if let imageData = data as? UIImage {
+ contentData = UIImagePNGRepresentation(imageData)
+ }
+ if let contentData = contentData{
+ let photo = Photo(data: contentData)
+ self.presentPhotoEditorController(photoAsset: photo)
+ }
+ }
+ })
+ 
+ imageFound = true
+ break
+ }
+ }
+ 
+ if (imageFound) {
+ // We only handle one image, so stop looking for more.
+ break
+ }
+ }
+ }
+ 
+ 
+ 
+ 
+ */
+
+
+
 extension LocationDetialViewController: PhotoEditViewControllerDelegate {
     
      func buildConfiguration() -> Configuration {
+        
         let configuration = Configuration() { builder in
             // Configure camera
             builder.configureCameraViewController() { options in
-                // Just enable Photos
+                /*******************************************/
+                options.includeUserLocation = true // See what this does
+                  /*******************************************/
                 options.allowedRecordingModes = [.photo]
             }
         }
@@ -45,13 +98,9 @@ extension LocationDetialViewController: PhotoEditViewControllerDelegate {
     }
 
     
-    
-    
-    
     func photoEditViewController(_ photoEditViewController: PhotoEditViewController, didSave image: UIImage, and data: Data) {
-        /*
-         t provides the resulting image as an UIImage and a Data object. Please note that the EXIF data of the input image is only fully contained within the Data object. Please refer to the next section for more information about EXIF handling.
- */
+        log.debug("Save image to photo album")
+        UIImageWriteToSavedPhotosAlbum(image, didSaveImage(photoEditViewController: photoEditViewController), nil, nil)
     }
     
     func photoEditViewControllerDidFailToGeneratePhoto(_ photoEditViewController: PhotoEditViewController) {
@@ -62,6 +111,17 @@ extension LocationDetialViewController: PhotoEditViewControllerDelegate {
         log.verbose("Canceled Editing")
     }
     
+    
+    // MARK: - UIImageWriteToSavedPhotosAlbum Completion Target
+    func didSaveImage(photoEditViewController: PhotoEditViewController){
+        log.debug("Save Alert")
+        let alert =  UIAlertController(title: "Saved", message: "Saved to Camera Roll", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: { (alertAction) in
+            // Return any edited content to the host app.
+            self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+        }))
+        photoEditViewController.present(alert, animated: true)
+    }
     
     func photoEditor(){
         let cameraViewController = CameraViewController()
